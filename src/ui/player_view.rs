8cb3a9a3eb9f43f5862c64558ded3ce7d674773a
Nate Jones <njones@arbor.net>
2020-10-03 17:52:33 -0400

    Formatting pass

--- src/ui/player_view.rs
+++ src/ui/player_view.rs
@@ -10,21 +10,15 @@ pub struct PlayerView {
 fn format_time(time: gst::ClockTime) -> String {
     let minutes = time.minutes().unwrap();
     let seconds = time.seconds().unwrap() % 60;
-    format!(
-        "{:02}:{:02}",
-        minutes,
-        seconds,
-    )
+    format!("{:02}:{:02}", minutes, seconds,)
 }
 
 impl View for PlayerView {
     fn draw(&self, printer: &Printer) {
-        let build_stream_length = || -> Option<String> {
-            Some(format_time(self.player.get_stream_length()?))
-        };
-        let build_stream_position = || -> Option<String> {
-            Some(format_time(self.player.get_stream_position()?))
-        };
+        let build_stream_length =
+            || -> Option<String> { Some(format_time(self.player.get_stream_length()?)) };
+        let build_stream_position =
+            || -> Option<String> { Some(format_time(self.player.get_stream_position()?)) };
 
         let playback_counter_string = format!(
             "{}/{}",
@@ -32,10 +26,7 @@ impl View for PlayerView {
             build_stream_length().unwrap_or("00:00".to_string()),
         );
 
-        printer.print(
-            (0, 0),
-            &playback_counter_string,
-        );
+        printer.print((0, 0), &playback_counter_string);
     }
 }
 
