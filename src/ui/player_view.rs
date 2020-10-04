be1cba181775a3a673e89d2e4e217ea0e7c9ebb1
Nate Jones <njones@arbor.net>
2020-10-04 11:40:19 -0400

    Simplify format_time()

--- src/ui/player_view.rs
+++ src/ui/player_view.rs
@@ -7,18 +7,18 @@ pub struct PlayerView {
     player: Player,
 }
 
-fn format_time(time: gst::ClockTime) -> String {
-    let minutes = time.minutes().unwrap();
-    let seconds = time.seconds().unwrap() % 60;
-    format!("{:02}:{:02}", minutes, seconds,)
+fn format_time(time: gst::ClockTime) -> Option<String> {
+    let minutes = time.minutes()?;
+    let seconds = time.seconds()? % 60;
+    Some(format!("{:02}:{:02}", minutes, seconds))
 }
 
 impl View for PlayerView {
     fn draw(&self, printer: &Printer) {
         let build_stream_length =
-            || -> Option<String> { Some(format_time(self.player.get_stream_length()?)) };
+            || -> Option<String> { format_time(self.player.get_stream_length()?) };
         let build_stream_position =
-            || -> Option<String> { Some(format_time(self.player.get_stream_position()?)) };
+            || -> Option<String> { format_time(self.player.get_stream_position()?) };
 
         let playback_counter_string = format!(
             "{}/{}",
