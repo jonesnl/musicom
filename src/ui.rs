b7426beba4f6e4abbfa9bc69231e56062e79dad0
Nate Jones <njones@arbor.net>
2020-10-11 16:20:27 -0400

    Reformat the PlayerView
    Turn off auto_refresh, you can use the cursive callback sink in order to
    trigger refreshes!
    
    TODO: Trigger refreshes only if we actually change any of the fields.

--- src/ui.rs
+++ src/ui.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 
 use cursive::view::Resizable;
 use cursive::views::LinearLayout;
+use cursive::Cursive;
 
 use crate::player::PlayerHdl;
 use crate::player::prelude::*;
@@ -24,9 +25,8 @@ impl UI {
     pub fn run(&mut self, dir: &Path) -> io::Result<()> {
         let mut siv = cursive::default();
 
-        let linear_layout = self.build_views(dir);
+        let linear_layout = self.build_views(&mut siv, dir);
         siv.add_fullscreen_layer(linear_layout);
-        siv.set_autorefresh(true);
 
         cursive::logger::init();
         let player_clone = self.player.clone();
@@ -37,13 +37,13 @@ impl UI {
         Ok(())
     }
 
-    fn build_views(&self, dir: &Path) -> LinearLayout {
+    fn build_views(&self, siv: &mut Cursive, dir: &Path) -> LinearLayout {
         let file_browser_view = self::file_browser::FileBrowserView::new(dir);
 
         let browser_layout = LinearLayout::horizontal().child(file_browser_view.full_width());
         //.child(DebugView::default().scrollable().scroll_strategy(cursive::view::ScrollStrategy::StickToBottom).full_width());
 
-        let player_bar = player_view::PlayerView::new();
+        let player_bar = player_view::PlayerView::new(siv);
         let top_level_layout = LinearLayout::vertical()
             .child(browser_layout.full_height())
             .child(player_bar.fixed_height(1).full_width());
--- src/ui/player_view.rs
+++ src/ui/player_view.rs
@@ -1,48 +1,92 @@
-use cursive::view::View;
-use cursive::Printer;
+use std::sync::{Arc, RwLock};
+
+use cursive::align::HAlign;
+use cursive::view::{ViewWrapper};
+use cursive::views::{DummyView, LinearLayout, TextContent, TextView};
+use cursive::traits::*;
+use cursive::Cursive;
+
+use gst::glib;
 
 use crate::player::prelude::*;
 use crate::player::PlayerHdl;
 
 pub struct PlayerView {
-    player: PlayerHdl,
+    linear_layout: LinearLayout,
 }
 
-fn format_time(time: gst::ClockTime) -> Option<String> {
-    let minutes = time.minutes()?;
-    let seconds = time.seconds()? % 60;
-    Some(format!("{:02}:{:02}", minutes, seconds))
-}
-
-impl View for PlayerView {
-    fn draw(&self, printer: &Printer) {
-        let build_stream_length =
-            || -> Option<String> { format_time(self.player.get_stream_length()?) };
-        let build_stream_position =
-            || -> Option<String> { format_time(self.player.get_stream_position()?) };
-
-        let tags = self.player.get_tag_list();
-        let title = if let Some(title) = tags.get::<gst::tags::Title>() {
-            title.get().unwrap_or("None").to_string()
-        } else {
-            "None".to_string()
-        };
-
-        let playback_counter_string = format!(
-            "{}/{}\t{}",
-            build_stream_position().unwrap_or("00:00".to_string()),
-            build_stream_length().unwrap_or("00:00".to_string()),
-            title,
-        );
-
-        printer.print((0, 0), &playback_counter_string);
+fn format_time(time: gst::ClockTime) -> String {
+    if time.is_none() {
+        return "00:00".to_string();
     }
+    let minutes = time.minutes().unwrap();
+    let seconds = time.seconds().unwrap() % 60;
+    format!("{:02}:{:02}", minutes, seconds)
+}
+
+impl ViewWrapper for PlayerView {
+    cursive::wrap_impl!(self.linear_layout: LinearLayout);
 }
 
 impl PlayerView {
-    pub fn new() -> Self {
+    pub fn new(siv: &Cursive) -> Self {
+        let stream_position = TextContent::new("HI");
+        let now_playing = TextContent::new("THERE");
+        let player_hdl = PlayerHdl::new();
+
+        let stream_position_copy = stream_position.clone();
+        let now_playing_copy = now_playing.clone();
+        let player_hdl_copy = player_hdl.clone();
+        let timeout_sink = siv.cb_sink().clone();
+        glib::timeout_add(100, move || {
+            let phc = &player_hdl_copy;
+            let position_string = match (phc.get_stream_length(), phc.get_stream_position()) {
+                (Some(len), Some(pos)) => {
+                    format!(
+                        "{}/{}",
+                        format_time(pos),
+                        format_time(len),
+                    )
+                },
+                _ => {"00:00/00:00".to_string()},
+            };
+
+            stream_position_copy.set_content(position_string);
+
+            let tags = player_hdl_copy.get_tag_list();
+            let title = if let Some(title) = tags.get::<gst::tags::Title>() {
+                title.get().unwrap_or("None").to_string()
+            } else {
+                "None".to_string()
+            };
+
+            now_playing_copy.set_content(
+                format!("Now Playing: \"{}\"", title)
+            );
+
+            timeout_sink.send(Box::new(cursive::Cursive::noop)).unwrap();
+            glib::Continue(true)
+        });
+
+        let mut linear_layout = LinearLayout::horizontal();
+        // DummyView is used to center the now_playing_view
+        linear_layout.add_child(
+            DummyView{}
+            .full_width()
+        );
+        linear_layout.add_child(
+            TextView::new_with_content(now_playing)
+            .h_align(HAlign::Center)
+            .full_width()
+        );
+        linear_layout.add_child(
+            TextView::new_with_content(stream_position)
+            .h_align(HAlign::Right)
+            .full_width()
+        );
+
         PlayerView {
-            player: PlayerHdl::new(),
+            linear_layout,
         }
     }
 }
