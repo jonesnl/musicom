b40aeee211d67b141d5288d46d53ba4a0a607f31
Nate Jones <njones@arbor.net>
2020-11-14 12:51:56 -0500

    Add some help text via ?

--- src/ui/queue_view.rs
+++ src/ui/queue_view.rs
@@ -1,17 +1,64 @@
 use cursive::align::HAlign;
+use cursive::direction::Direction;
+use cursive::event::{AnyCb, Event, EventResult};
 use cursive::traits::*;
-use cursive::view::ViewWrapper;
+use cursive::view::Selector;
+use cursive::views::Dialog;
 use cursive::views::SelectView;
+use cursive::{Printer, Rect, Vec2};
 
 use crate::player::PlayerHdl;
+
+const HELP_TEXT: &'static str = "\
+Press <p> to pause/play the current song";
 
 pub struct QueueView {
     select_view: SelectView,
     player: PlayerHdl,
 }
 
-impl ViewWrapper for QueueView {
-    cursive::wrap_impl!(self.select_view: SelectView);
+impl View for QueueView {
+    fn draw(&self, printer: &Printer) {
+        self.select_view.draw(printer);
+    }
+
+    fn layout(&mut self, xy: Vec2) {
+        self.select_view.layout(xy);
+    }
+
+    fn needs_relayout(&self) -> bool {
+        self.select_view.needs_relayout()
+    }
+
+    fn required_size(&mut self, constraint: Vec2) -> Vec2 {
+        self.select_view.required_size(constraint)
+    }
+
+    fn on_event(&mut self, e: Event) -> EventResult {
+        match e {
+            Event::Char('?') => EventResult::with_cb(move |siv| {
+                let popup = Self::get_help_view();
+                siv.add_layer(popup);
+            }),
+            _ => self.select_view.on_event(e)
+        }
+    }
+
+    fn call_on_any<'a>(&mut self, s: &Selector<'_>, cb: AnyCb<'a>) {
+        self.select_view.call_on_any(s, cb);
+    }
+
+    fn focus_view(&mut self, s: &Selector<'_>) -> Result<(), ()> {
+        self.select_view.focus_view(s)
+    }
+
+    fn take_focus(&mut self, source: Direction) -> bool {
+        self.select_view.take_focus(source)
+    }
+
+    fn important_area(&self, view_size: Vec2) -> Rect {
+        self.select_view.important_area(view_size)
+    }
 }
 
 impl QueueView {
@@ -38,6 +85,10 @@ impl QueueView {
             }));
 
         qv.with_name("queue_view")
+    }
+
+    fn get_help_view() -> impl View {
+        Dialog::info(HELP_TEXT)
     }
 
     fn refresh_view(&mut self) {
